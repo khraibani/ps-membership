eb78c4150fd03ad701f63fc2eee40072a5430bce
Hussein Khraibani <sein.khraibani@gmail.com>
2016-01-09 13:21:02 +0000

    Initial

---
+++ test/sample.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe("Something", function(){
+    describe("This smells", function(){
+        it("is pungent", function(){
+                assert.ok(true);
+            });
+    });
+});
