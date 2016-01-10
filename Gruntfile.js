b567132e1b4e1140bc3701a3648320178d506dff
Hussein Khraibani <sein.khraibani@gmail.com>
2016-01-10 13:04:13 +0000

    install secondthought package a light wrapper for RethinkDB - gruntfile to installDB

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,6 @@
+var db = require("secondthought");
+var assert = require("assert");
+
 module.exports = function(grunt) {
     grunt.initConfig({
         jshint: {
@@ -8,6 +11,17 @@ module.exports = function(grunt) {
             files: ['lib/**/*js', 'models/**/*.js'],
             tasks: ['jshint']
         }
+    });
+
+    grunt.registerTask("installDB", function(){
+        var done = this.async();
+        db.connect({db : "membership"}, function(err, db){
+            db.install(['users', 'logs', 'sessions'], function(err, tableResult){
+                assert.ok(err === null, err);
+                console.log("DB installed: " + tableResult);
+                done();
+            });
+        });
     });
 
     grunt.loadNpmTasks("grunt-contrib-jshint");
